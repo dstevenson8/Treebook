80fd27f75ed2b6da22359959d9516a21de542c13
David Stevenson <DavidStevenson@Davids-MacBook-Air.local>
2013-04-16 21:25:12 -0400

    testing commit

---
+++ test_make/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestMake::Application.config.secret_token = 'f40d472a0e31582331643101aaf8bbc20797849b54d757dfe930710333c79c5b4f977f9b6673be8e880f24cd3a3e666e8714d2d909608ebbcb2ca7d100a2b54e'
---
+++ treebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'bba861c0105d50f1d9055d0834a4b943846ffe9160727c0d4ce0ab0059ca9e6963b31eafe61b113a304e25854c4381101e2a6399c0dee92c15f75ac95b8b0852'
